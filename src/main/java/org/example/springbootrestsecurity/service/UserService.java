4d2dbcd169dd0dbc21dc686ef52133b162a7f0c6
Aliaksutkin_Aliaksandr <aleksutkinaliaksandr@gmail.com>
2021-11-29 23:09:13 +0300

    controller

---
+++ src/main/java/org/example/springbootrestsecurity/service/UserService.java
@@ -0,0 +1,2 @@
+package org.example.springbootrestsecurity.service;public interface UserService {
+}
---
+++ src/main/java/org/example/springbootrestsecurity/service/UserServiceImpl.java
@@ -0,0 +1,2 @@
+package org.example.springbootrestsecurity.service;public class UserServiceImpl {
+}
