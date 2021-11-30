a70bd8244ba1d49842a47374a9074cb905e43cb8
Aliaksutkin_Aliaksandr <aleksutkinaliaksandr@gmail.com>
2021-11-30 22:43:23 +0300

    1

---
+++ src/main/java/org/example/springbootrestsecurity/controller/AuthenticationRestControllerV1.java
@@ -0,0 +1,26 @@
+package org.example.springbootrestsecurity.rest;
+
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationRestControllerV1 {
+
+
+
+
+
+
+
+    @PostMapping("/logout")         //todo 
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        securityContextLogoutHandler.logout(request, response, null);
+    }
+}
---
+++ src/main/java/org/example/springbootrestsecurity/security/JwtTokenProvider.java
@@ -0,0 +1,2 @@
+package org.example.springbootrestsecurity.security;public class JwtTokenProvider {
+}
