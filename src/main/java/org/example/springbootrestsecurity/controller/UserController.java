3cc2987de89b7ab4471a1fb7d3289ebe4b5f5c8d
Aliaksutkin_Aliaksandr <aleksutkinaliaksandr@gmail.com>
2021-12-02 10:37:57 +0300

    раздвоение

--- src/main/java/org/example/springbootrestsecurity/controller/UserController.java
+++ src/main/java/org/example/springbootrestsecurity/controller/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-    @Secured(value = {"hasAnyAuthority('users:read')"})
+    @Secured("hasAnyAuthority('users:read')")
     @GetMapping("/homepage")
     public List<User> viewHomePage() {
         return userService.getAllUsers();
